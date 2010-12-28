e7f84be28d70ae9c346b0296d17467ce2be681ee
Francis Morissette <owner@gta-sa-mp.com>
2010-12-28 13:56:35 -0500

    All Module_*.php in modules will be loaded and the class * will be initiated.

--- global.php
+++ global.php
@@ -34,4 +34,17 @@ foreach($coreFiles as $file) {
 	$Core[$className] = new $className();
 }
 
+$Modules = array();
+$moduleFiles = glob(dirname(__FILE__) . "/modules/Module_*.php");
+foreach($moduleFiles as $file) {
+	preg_match("/Module_(.*)\.php/", $file, $className);
+	
+	$className = $className[1];	
+	
+	// Let's load all classes, they will still need to be initiated manually though.
+	// Highly impossible to die here, but let's play safe.
+	@(include($file)) or die("Couldn't load modules/" . $className . ".php.");
+	$Modules[$className] = new $className();
+}
+
 ?>
